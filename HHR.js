32fb4f5d0afa23c4526eed5356a053315fe659af
rrs12 <77575080+rrs12@users.noreply.github.com>
2021-07-03 14:51:12 +0530

    Add files via upload

---
+++ HHR.js
@@ -0,0 +1,34 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAqs7XiLi7zRml6QtKqnUPnZGNqHxMllxs",
+    authDomain: "hhr1website.firebaseapp.com",
+    databaseURL: "https://hhr1website-default-rtdb.firebaseio.com",
+    projectId: "hhr1website",
+    storageBucket: "hhr1website.appspot.com",
+    messagingSenderId: "656572968982",
+    appId: "1:656572968982:web:79d494b3a1cde972b98537"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+user_no= document.getElementById("no_user").value;
+email_user= document.getElementById("email_user").value;
+
+room_name="123456"
+function submit(){
+  document.getElementById("ask_number").style.display="none"
+  document.getElementById("thanks").style.display="inherit"
+buy_book=localStorage.getItem("book")
+  user_no= document.getElementById("no_user").value;
+email_user= document.getElementById("email_user").value;
+    firebase.database().ref(room_name).push({
+        Phone_no: user_no,
+        email_of_user: email_user,
+        book: buy_book
+  })
+
+}
+function buyBook(clicked_me){
+localStorage.setItem("book", clicked_me)
+window.location= "GetBook.html"
+}
+
---
+++ owl_carousel.js
@@ -0,0 +1,23 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    padding:15,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
